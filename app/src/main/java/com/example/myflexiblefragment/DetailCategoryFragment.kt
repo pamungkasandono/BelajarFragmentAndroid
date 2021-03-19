9aa65653132da321b99965b20775be402ab2aa88
pamungkas andono <pamungkas.org@gmail.com>
2021-03-19 21:39:27 +0700

    Tambah activity untuk profile

--- app/src/main/java/com/example/myflexiblefragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/example/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,7 @@
 package com.example.myflexiblefragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -40,6 +42,10 @@ class DetailCategoryFragment : Fragment() {
             val mOptionDialogFragment = OptionDialogFragment()
             val mFragmentManager = childFragmentManager
             mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+        btnProfile.setOnClickListener {
+            val mIntent = Intent(activity, ProfileActivity::class.java)
+            startActivity(mIntent)
         }
     }
 
